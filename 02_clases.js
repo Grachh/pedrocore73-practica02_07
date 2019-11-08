eea0c362f77046fcddb665b5a94493c0da30a10a
pedrocore73 <pjimenez@corenetworks.es>
2019-11-08 20:43:06 +0100

    clases

--- 02_clases.js
+++ 02_clases.js
@@ -1,4 +1,17 @@
 /* Clases en TypeScript */
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Jugador = /** @class */ (function () {
     function Jugador(nombre, apellidos, dorsal) {
         this.nombre = nombre;
@@ -15,3 +28,81 @@ var Jugador = /** @class */ (function () {
 var jugador1 = new Jugador('Sergio', 'Ramos', 4);
 console.log(jugador1);
 // console.log(jugador1.creadoEl) provoca error al ser la propiedad privada
+// Clases con constructor breve
+var Player = /** @class */ (function () {
+    function Player(nombre, apellidos, dorsal, creadoEl, goles) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dorsal = dorsal;
+        this.creadoEl = creadoEl;
+        this.goles = goles;
+        this.goles = goles ? goles : 0;
+    }
+    Player.prototype.sumaGol = function () {
+        this.goles++;
+    };
+    return Player;
+}());
+var player1 = new Player('Cristiano', 'Ronaldo', 9, new Date());
+console.log(player1);
+// Herencia de clases
+var Empleado = /** @class */ (function () {
+    function Empleado(nombre, tfnoMovil, edad) {
+        this.nombre = nombre;
+        this.tfnoMovil = tfnoMovil;
+        this.edad = edad ? edad : 0;
+    }
+    Empleado.prototype.getNombre = function () {
+        return this.nombre;
+    };
+    Empleado.prototype.setNombre = function (nombre) {
+        this.nombre = nombre;
+    };
+    Empleado.prototype.setEdadCumple = function () {
+        this.edad++;
+    };
+    return Empleado;
+}());
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager(nombre, tfnoMovil, edad, rol) {
+        var _this = _super.call(this, nombre, tfnoMovil, edad) || this;
+        _this.rol = rol;
+        return _this;
+    }
+    Manager.prototype.setPrefijo = function (prefijo) {
+        this.tfnoMovil = prefijo + this.tfnoMovil; // no da error porque 
+        // la propiedad es protegida en la madre
+    };
+    return Manager;
+}(Empleado));
+var posicion = new Manager('Juan', '666666666', 40, 'ventas');
+// console.log(posicion.edad) // Error por ser privada
+// console.log(posicion.tfnoMovil); // Error por ser protegida
+console.log(posicion.rol);
+posicion.setPrefijo('+34');
+console.log(posicion);
+// Composición de clases
+var Alumno = /** @class */ (function () {
+    function Alumno(nombre, dni) {
+        this.direcciones = [];
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+    Alumno.prototype.setDireccionAlumno = function (direccion) {
+        this.direcciones.push(direccion);
+    };
+    return Alumno;
+}());
+var Direccion = /** @class */ (function () {
+    function Direccion(calle, localidad) {
+        this.calle = calle;
+        this.localidad = localidad;
+    }
+    return Direccion;
+}());
+var cliente1 = new Alumno('Juan', '07454154V');
+var direccion1 = new Direccion('Serrano Galvache, 56', 'Madrid');
+cliente1.setDireccionAlumno(direccion1);
+cliente1.setDireccionAlumno({ calle: 'Ppe Vergara, 100', localidad: 'Madrid' });
+console.log(cliente1);
--- 02_clases.ts
+++ 02_clases.ts
@@ -25,3 +25,113 @@ class Jugador {
 let jugador1 = new Jugador('Sergio','Ramos',4);
 console.log(jugador1);
 // console.log(jugador1.creadoEl) provoca error al ser la propiedad privada
+
+// Clases con constructor breve
+
+class Player {
+
+    constructor(public nombre: string,
+                public apellidos: string,
+                public dorsal: number,
+                private creadoEl: object,
+                public goles?:number,
+               ) {
+                   this.goles = goles ? goles : 0;
+               }
+
+    sumaGol(): void {
+        this.goles++;
+    }
+
+}
+
+let player1 = new Player('Cristiano','Ronaldo',9,new Date());
+console.log(player1);
+
+// Herencia de clases
+
+class Empleado {
+    public nombre: string;
+    protected tfnoMovil: string; // para la clase y sus clases hijas
+    private edad: number;
+
+    constructor(nombre: string, tfnoMovil: string, edad: number){
+        this.nombre = nombre;
+        this.tfnoMovil = tfnoMovil;
+        this.edad = edad ? edad : 0;
+    }
+
+    getNombre() {
+        return this.nombre;
+    }
+
+    setNombre(nombre: string) {
+        this.nombre = nombre;
+    }
+
+    setEdadCumple() {
+        this.edad++;
+    }
+}
+
+class Manager extends Empleado {
+
+    public rol: string;
+    
+    constructor(nombre: string, tfnoMovil: string, edad: number, rol: string ) {
+        super(nombre, tfnoMovil, edad);
+        this.rol = rol;
+    }
+
+    setPrefijo(prefijo: string) {
+        this.tfnoMovil = prefijo + this.tfnoMovil; // no da error porque 
+                                                  // la propiedad es protegida en la madre
+    }
+
+    // setEdadCumple() {
+    //     this.edad++; // Lanza error porque la propiedad es privada en la madre
+    // }
+
+}
+
+let posicion = new Manager('Juan','666666666', 40,'ventas');
+// console.log(posicion.edad) // Error por ser privada
+// console.log(posicion.tfnoMovil); // Error por ser protegida
+console.log(posicion.rol);
+posicion.setPrefijo('+34');
+console.log(posicion);
+
+// Composición de clases
+
+class Alumno {
+    public nombre: string;
+    public dni: string;
+    public direcciones: Array<any> = [];
+
+    constructor(nombre: string, dni: string) {
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+
+    setDireccionAlumno(direccion: Direccion) {
+        this.direcciones.push(direccion);
+    }
+
+}
+
+class Direccion {
+    public calle: string;
+    public localidad: string;
+
+    constructor(calle: string, localidad: string) {
+        this.calle = calle;
+        this.localidad = localidad;
+    }
+}
+
+let cliente1 = new Alumno('Juan','07454154V');
+let direccion1 = new Direccion('Serrano Galvache, 56', 'Madrid');
+
+cliente1.setDireccionAlumno(direccion1);
+cliente1.setDireccionAlumno({calle:'Ppe Vergara, 100',localidad:'Madrid'});
+console.log(cliente1);
